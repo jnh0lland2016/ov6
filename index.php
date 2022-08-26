5ca5493d77728729bcafab67f2ab8db078f643c5
jnh0lland2016 <108730408+jnh0lland2016@users.noreply.github.com>
2022-08-26 12:58:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
+<head>
+<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js?ver=1.4'></script>
 
-<?php 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://dbm2consulting.com/OV6/index.php");
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-$result = curl_exec($ch);
-curl_close($ch);
-$result = preg_replace("#(<\s*a\s+[^>]*href\s*=\s*[\"'])(?!http)([^\"'>]+)([\"'>]+)#",'$1http://www.your_external_website.com/$2$3', $result);
-echo $result
-
-
-?>
-
-
+<script type='text/javascript'>
+$(document).ready(function (){
+$('#divId').load('https://dbm2consulting.com/OV6/');     
+});
+</script>
+</head>
