48cce5a898f50b99840f63f195661cfe18826d86
jnh0lland2016 <108730408+jnh0lland2016@users.noreply.github.com>
2022-08-26 12:41:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 
 <?php 
-function getter($url) {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    $data = curl_exec($ch);
-    curl_close($ch);
-    return $data;
-}
-
-echo getter('https://dbm2consulting.com/OV6/index.php');
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://dbm2consulting.com/OV6/index.php");
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$result = curl_exec($ch);
+curl_close($ch);
+$result = preg_replace("#(<\s*a\s+[^>]*href\s*=\s*[\"'])(?!http)([^\"'>]+)([\"'>]+)#",'$1http://www.your_external_website.com/$2$3', $result);
+echo $result
 
 
 ?>
+
+
